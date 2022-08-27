76ab12760ecc75336bf63230e7056795fa2ecd1a
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-08-28 01:49:14 +0900

    emmet init

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,12 @@
 import { EditorState } from "@codemirror/state";
 import { EditorView } from "@codemirror/view";
+import { html } from "@codemirror/lang-html";
+import { abbreviationTracker } from "@emmetio/codemirror6-plugin";
 
 new EditorView({
   state: EditorState.create({
     doc: "hello",
+    extensions: [html(), abbreviationTracker()],
   }),
   parent: document.getElementById("codemirror"),
 });
