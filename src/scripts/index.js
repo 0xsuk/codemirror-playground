9c22615023e1a3d50d5446b418b67ba10ec7c53e
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-08-28 00:35:49 +0900

    use npm

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,9 @@
 import { EditorState } from "@codemirror/state";
+import { EditorView } from "@codemirror/view";
 
-console.log(EditorState);
+new EditorView({
+  state: EditorState.create({
+    doc: "hello",
+  }),
+  parent: document.getElementById("codemirror"),
+});
