47bd87d9b67c91195e0a85e6202acf12cb52e51d
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-08-29 04:46:06 +0900

    del

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { EditorState } from "@codemirror/state";
-import { EditorView } from "@codemirror/view";
-
-new EditorView({
-  state: EditorState.create({
-    doc: "hello",
-  }),
-  parent: document.getElementById("codemirror") as HTMLElement,
-});
